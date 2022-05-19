005788fa1de4d8efb24862773ec39e0a755ad886
MidnQP <midnightquantumprogrammer@gmail.com>
2022-05-19 21:26:12 +0600

    fix(util): correct indent for debug logs

--- src/util.ts
+++ src/util.ts
@@ -1,45 +1,43 @@
 import chalk from 'chalk'
 import process from 'process'
 import util from 'util'
-import { URL } from 'url'
+import {URL} from 'url'
 import axios from 'axios'
 import * as types from './typings/index.js'
 import lodash from 'lodash'
 import dotenv from 'dotenv'
-export { lodash }
+export {lodash}
 
 dotenv.config()
 const pcolibLogLevel = process.env.pcolibLogLevel || 'none'
 const pcolibLogIndent = Number(process.env.pcolibLogIndent || '0')
 
 /** @throws {Error} - If `item` is undefined. */
-export function check (item: any, entity): item is Record< string, any > {
+export function check (item: any, entity): item is Record<string, any> {
 	if (item === undefined) throw Error(entity + ' not found.')
 	return true
 }
 
 /** Mostly used to traverse to child folders/requests/examples. */
-export function next_where (
-	array: Record< string, string >[],
-	value,
-	prop = 'name'
-) {
+export function next_where (array: Record<string, string>[], value, prop = 'name') {
 	return array.find(each => each[prop].toLowerCase() === value.toLowerCase())
 }
 
 export const log = {
 	/** plain debug info */
-	debug: function (msg: string|object|number, indent=0) {
-		if ( ['debug'].includes(pcolibLogLevel)) {
-			if ( lodash.isObject(msg)) msg = expand( msg )
+	debug: function (msg: string | object | number, indent = 0) {
+		if (['debug'].includes(pcolibLogLevel)) {
+			if (lodash.isObject(msg)) msg = expand(msg)
 
-			msg = ''+msg
-			const marker = chalk.cyanBright('debug')+' '
-			if (pcolibLogIndent) indent=pcolibLogIndent
+			msg = '' + msg
+			const _marker = 'debug'
+			const marker = chalk.cyanBright(_marker) + ' '
+			if (pcolibLogIndent) indent = pcolibLogIndent
 			const tbsp = ' '.repeat(indent)
 
-			msg = msg.replace(/\n/g, '\n'+tbsp+' '.repeat(marker.length))
-			process.stdout.write(tbsp+marker+chalk.white(msg)+'\n')
+			const rpl = '\n' + tbsp + ' '.repeat(_marker.length)
+			msg = msg.replace(/\n/g, rpl)
+			process.stdout.write(tbsp + marker + chalk.white(msg) + '\n')
 		}
 	},
 }
@@ -48,11 +46,7 @@ export const log = {
  * Destructures an array of obj
  * by key & value.
  */
-function arr_destr (
-	obj: Record< string, any >[],
-	key = 'key',
-	value = 'value'
-) {
+function arr_destr (obj: Record<string, any>[], key = 'key', value = 'value') {
 	const params = {}
 	if (!Array.isArray(obj)) return {}
 	for (const i of obj) params[i[key]] = i[value]
@@ -73,8 +67,8 @@ export function replace_postman_var (url: string, variableStore) {
  * Replaces path variables placeholders
  * with their values.
  */
-export function replace_path_var (url: string, paths: Record< string, any >) {
-	Object.entries(paths).forEach(([ k, v ]) => {
+export function replace_path_var (url: string, paths: Record<string, any>) {
+	Object.entries(paths).forEach(([k, v]) => {
 		url = url.replace(`:${k}`, v)
 	})
 	return url
@@ -111,6 +105,6 @@ export function normalize (normalizable: types.InputNormData): types.NormData {
 }
 
 export function expand (obj: object) {
-	const opts = { indentationLvl: 2, colors: true, depth: 20 }
+	const opts = {indentationLvl: 2, colors: true, depth: 20}
 	return util.inspect(obj, opts)
 }
